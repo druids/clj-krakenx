605bbcbc7a97a2a9359a94e847af9f645b7b7337
Lukas Rychtecky <lukas.rychtecky@gmail.com>
2018-03-12 14:01:47 +0100

    0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-krakenx "0.1.0"
+(defproject clj-krakenx "0.2.0"
   :description "A client for Kraken Bitcoin Exchange API based on clj-http.client"
   :url "https://github.com/druids/clj-krakenx"
   :license {:name "MIT License"
